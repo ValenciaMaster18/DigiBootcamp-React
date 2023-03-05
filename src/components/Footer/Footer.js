617af18d667d33be01e15a0631866a342eb5a820
Luis valencia <luchito123410@gmail.com>
2023-03-04 20:28:58 -0500

    Creamos el footer de la page

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import "./Footer.css";
+
+export function Footer() {
+    return (
+        // <!-- Footer -->
+        <footer>
+            {/* Copyright */}
+            <div>
+                © 2023 Copyright:
+                <a href="https://github.com/ValenciaMaster18/DigiBootcamp-React">CNUNO-DEVS</a>
+            </div>
+        </footer>
+    )
+}
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,6 +6,7 @@ import { Portada } from "../../components/Portada/Portada";
 import { About } from "../../components/About/About";
 import { Servicios } from "../../components/Servicios/Servicios";
 import { Contact } from "../../components/Contact/Contact";
+import { Footer } from "../../components/Footer/Footer";
 // Para trbajar con iconos de react (https://react-icons.github.io/react-icons/icons?name=fa)
 import { FaArrowUp } from "react-icons/fa";
 
@@ -18,6 +19,7 @@ function Home(){
             <About /> 
             <Servicios />
             <Contact />
+            <Footer />
             <div className="home__button">
                 <a href="#header">
                     <button className="button__subir">
